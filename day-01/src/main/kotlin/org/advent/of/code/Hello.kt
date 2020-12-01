cb24dd0f7b5a6936720041b8204e4ef1cfcd4418
Benoit Luttringer <benoit.luttringer@gmail.com>
2020-12-01 12:00:51 +0100

    Adding day-01

---
+++ day-01/src/main/kotlin/org/advent/of/code/Hello.kt
@@ -0,0 +1,6 @@
+package org.advent.of.code
+
+fun main(args: Array<String>) {
+    println("Hello, World")
+}
+
---
+++ day-01/src/test/kotlin/org/advent/of/code/HelloTest.kt
@@ -0,0 +1,8 @@
+package org.advent.of.code
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}
